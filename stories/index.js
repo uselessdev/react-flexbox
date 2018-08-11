5d07277c51d993d874a47aa62b351a11f9826419
Wallace Oliveira <wallace.oliveira@pagar.me>
2018-08-11 20:36:17 -0300

    stories: add default stories

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,39 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Component from '../src'
+import {
+  Col,
+  Grid,
+} from '../src'
 
-storiesOf('Component', module)
-  .add('with text', () => <Component>Welcome to creator</Component>)
+const Text = ({ children }) => (
+  <span
+    style={{
+      width: '100%',
+      display: 'block',
+      marginTop: 10,
+      marginBottom: 10,
+      color: 'white',
+      lineHeight: 3,
+      textAlign: 'center',
+      borderRadius: 4,
+      backgroundColor: 'slateblue',
+    }}
+  >
+    { children }
+  </span>
+)
+
+storiesOf('Grid', module)
+  .add('default', () => (
+    <Grid>
+      <Col><Text>full size column</Text></Col>
+      <Col size={1/2}><Text>1/2</Text></Col>
+      <Col size={1/2}><Text>1/2</Text></Col>
+      <Col size={1/3} small={1/2}><Text>1/3 | small 1/2</Text></Col>
+      <Col size={1/3} small={1/2}><Text>1/3 | small 1/2</Text></Col>
+      <Col size={1/3} small={1}><Text>1/3 | small 1</Text></Col>
+      <Col medium={1/2}><Text>1 | medium 1/2</Text></Col>
+      <Col medium={1/2}><Text>1 | medium 1/2</Text></Col>
+    </Grid>
+  ))
